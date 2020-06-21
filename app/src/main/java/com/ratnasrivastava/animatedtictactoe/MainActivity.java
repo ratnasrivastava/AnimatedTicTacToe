27d9afeb9a6ba9fcdeb1709ce0edc18de79e7c9f
Ratna Srivastava <er.ratnasrivastava@gmail.com>
2020-06-21 22:18:04 +0530

    MODIFIED :: Alignment Correction

--- app/src/main/java/com/ratnasrivastava/animatedtictactoe/MainActivity.java
+++ app/src/main/java/com/ratnasrivastava/animatedtictactoe/MainActivity.java
@@ -11,14 +11,15 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-public static final String TAG = "unique7";
-private boolean player1 = true;
-private boolean gameActive = true;
-private int[] gameBoard = {2,2,2,2,2,2,2,2,2};
-private int[][] winningSituations = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
-TextView winnerTextView;
+    public static final String TAG = "unique7";
+    TextView winnerTextView;
+    private boolean player1 = true;
+    private boolean gameActive = true;
+    private int[] gameBoard = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    private int[][] winningSituations = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
     Button playAgainButton;
     private static int count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +30,7 @@ TextView winnerTextView;
 
     public void dropIn(View view) {
         count++;
-        ImageView imageView = (ImageView) view;
+        final ImageView imageView = (ImageView) view;
         int tag = Integer.parseInt(imageView.getTag().toString());
         if (gameActive && gameBoard[tag] == 2) {
             imageView.setTranslationY(-1500);
@@ -63,9 +64,9 @@ TextView winnerTextView;
     }
 
     private boolean checkForWin() {
-        for(int win[] : winningSituations){
-            if(gameBoard[win[0]] != 2 && gameBoard[win[0]] == gameBoard[win[1]] && gameBoard[win[0]] == gameBoard[win[2]]){
-                return  true;
+        for (int win[] : winningSituations) {
+            if (gameBoard[win[0]] != 2 && gameBoard[win[0]] == gameBoard[win[1]] && gameBoard[win[0]] == gameBoard[win[2]]) {
+                return true;
             }
         }
         return false;
@@ -73,10 +74,10 @@ TextView winnerTextView;
 
     public void playAgain(View view) {
         GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
-        for(int i=0;i<gridLayout.getChildCount();i++){
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
             ((ImageView) gridLayout.getChildAt(i)).setImageDrawable(null);
         }
-        for(int i=0;i<9;i++){
+        for (int i = 0; i < 9; i++) {
             gameBoard[i] = 2;
         }
         winnerTextView.setVisibility(View.INVISIBLE);
